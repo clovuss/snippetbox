8569a95517a8d50dd82faa6c182574fc7380bf9f
clovus <clovuss@yandex.ru>
2021-11-19 19:45:47 +0400

    added flags

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -33,16 +34,17 @@ func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
 	return file, nil
 }
 func main() {
-
+	addr := flag.String("addr", ":8080", "номер порта на котором запускается веб-сервер")
+	flag.Parse()
 	router := http.NewServeMux()
 	router.HandleFunc("/", home)
 	router.HandleFunc("/snippet", showSnippet)
 	router.HandleFunc("/snippet/create", createSnippet)
-	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./static")})
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static")})
 	router.Handle("/static/", http.StripPrefix("/static", fileServer))
-	log.Println("Запуск сервера на 127.0.0.1:8080")
+	log.Printf("Запуск сервера на 127.0.0.1:%s", *addr)
 
-	err := http.ListenAndServe(":8080", router)
+	err := http.ListenAndServe(*addr, router)
 
 	if err != nil {
 		fmt.Println(err)
